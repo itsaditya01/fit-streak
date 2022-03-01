5c490794368a7c786cc50c4eeba3701fedacee38
itsaditya01 <adityachauhan9974@gmail.com>
2022-03-01 23:31:42 +0530

    Exercise assigning json

---
+++ src/helper/WebData.js
@@ -0,0 +1,476 @@
+// exercise naming convention : neck_rotation, squat, sprawl, plank, pushup, mountain_climb, lunges, warrior_one, warrior_two
+
+let WebData = {
+  moreThan40: {
+    1: {
+      exercise: [
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "squat",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "sprawl",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "squat",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "plank",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+      ],
+    },
+    2: {
+      exercise: [
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "pushup",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "montain_climb",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "pushup",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "plank",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+      ],
+    },
+    3: {
+      exercise: [
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "lunges",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "high_knee",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "lunges",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "plank",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+      ],
+    },
+    4: {
+      exercise: [
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "squat",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "sparwl",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "squat",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "plank",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+      ],
+    },
+    5: {
+      exercise: [
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "squat",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "warrior_one",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "squat",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "plank",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+      ],
+    },
+    1: {
+      exercise: [
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "pushup",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "warrior_two",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "pushup",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "plank",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+      ],
+    },
+  },
+  lessThan40: {
+    1: {
+      exercise: [
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "squat",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "sprawl",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "squat",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "plank",
+          sets: 1,
+          reps: 5,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 5,
+        },
+      ],
+    },
+    2: {
+      exercise: [
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "pushup",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "montain_climb",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "pushup",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "plank",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+      ],
+    },
+    3: {
+      exercise: [
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "lunges",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "high_knee",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "lunges",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "plank",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+      ],
+    },
+    4: {
+      exercise: [
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "squat",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "sparwl",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "squat",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "plank",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+      ],
+    },
+    5: {
+      exercise: [
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "squat",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "warrior_one",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "squat",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "plank",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+      ],
+    },
+    6: {
+      exercise: [
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "pushup",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "warrior_two",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "pushup",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "plank",
+          sets: 1,
+          reps: 10,
+        },
+        {
+          name: "neck_rotation",
+          sets: 1,
+          reps: 10,
+        },
+      ],
+    },
+  },
+};
