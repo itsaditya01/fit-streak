85a515af1a683c30d5f30f8d9fbab447b5a02c06
Pratham Shah <spratham053@gmail.com>
2022-03-02 13:25:31 +0530

    Hero section added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,3 +1,5 @@
+import { WebData } from "../../data/WebData";
+import Fire from "../../assets/Fire.png";
 import "./hero.css";
 
 const Hero = () => {
@@ -8,6 +10,21 @@ const Hero = () => {
           <h1 style={{ color: "var(--main-color)" }}>
             Here are your daily goals
           </h1>
+          <ul>
+            {WebData.lessThan40.day1.exercise.map((val) => (
+              <li
+                style={{ color: "white" }}
+              >{`${val.name} ${val.reps} Reps and ${val.sets} Set`}</li>
+            ))}
+          </ul>
+          <div className="streak-div">
+            <h1 style={{ color: "white" }}>3 Day Streak</h1>
+            <img
+              src={Fire}
+              alt="Streak"
+              style={{ height: "auto", width: 50 }}
+            />
+          </div>
           <p style={{ color: "white" }}>
             Complete the daily exercises and flex in front of your friends
           </p>
@@ -16,7 +33,11 @@ const Hero = () => {
             <button className="room-btn">Join your friends</button>
           </div>
         </div>
-        <div className="friend-acheivments"></div>
+        <div className="friend-acheivments">
+          <h1 style={{ color: "var(--main-color)" }}>
+            These friends are on a journey of self improvement
+          </h1>
+        </div>
       </div>
       <div className="right-hero">Hello</div>
     </div>
