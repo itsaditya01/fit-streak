f4583e261f7768f685fd9425492e3efb9ef5f643
Pratham Shah <spratham053@gmail.com>
2022-03-03 19:13:36 +0530

    search bar changed

--- src/RightSideBar/RightBar.jsx
+++ src/RightSideBar/RightBar.jsx
@@ -6,58 +6,98 @@ import Friend1 from "./people5.jpg";
 import Friend2 from "../assets/people1.jpg";
 import Friend3 from "../assets/people2.jpg";
 import Friend4 from "../assets/people3.jpeg";
-import { useAuth } from "../context/AuthContext"
+import { useAuth } from "../context/AuthContext";
+import search from "../assets/search.svg";
 const RightBar = () => {
-
-  const { currentUser } = useAuth()
+  const { currentUser } = useAuth();
   const [friend_profile, setFriend_profile] = useState(null);
   const [own_profile, setOwn_profile] = useState({});
   useEffect(() => {
     const owndata = async () => {
       const response = await fetch(`http://localhost:5000/api/getuser`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          email: currentUser.email
-        })
-      })
+          email: currentUser.email,
+        }),
+      });
       const data = await response.json();
       setOwn_profile(data);
-    }
+    };
     owndata();
-  }, [])
-  const friendusername = useRef()
+  }, []);
+  const friendusername = useRef();
   const frienddata = async () => {
     const response = await fetch(`http://localhost:5000/api/searchfriend`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        username: friendusername.current.value
-      })
-    })
+        username: friendusername.current.value,
+      }),
+    });
     const data = await response.json();
     setFriend_profile(data);
-  }
-
+  };
 
   return (
     <>
       <div className="rightbar df ac fdc">
         <div className="friends-list">
-          <p className="rightbar-heading"> FRIENDS</p>
-          <div className="search-bar" style={{ display: "flex", flexDirection: "row", marginBottom: "25px", }}>
-            <input type="text" ref={friendusername} id="search" placeholder="Search Friends" style={{ color: "white", marginRight: "5px", padding: '5px', fontSize: 14 }}></input>
-            <button type="submit" id="search-btn" style={{ padding: "0px 3px", fontSize: 13 }} onClick={frienddata}>Search</button>
+          <div
+            className="search-bar"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              marginBottom: "25px",
+            }}
+          >
+            <input
+              type="text"
+              ref={friendusername}
+              id="search"
+              placeholder="Search Friends"
+              style={{
+                color: "white",
+                marginRight: "5px",
+                padding: "5px 5px 5px 10px",
+                fontSize: 14,
+              }}
+            ></input>
+            <img
+              src={search}
+              alt="Search"
+              id="search-btn"
+              // style={{ padding: "0px 3px", fontSize: 13 }}
+              onClick={frienddata}
+            />
           </div>
-          {friend_profile !== null ? <Friends
-            friend_name={friend_profile.username}
+          {friend_profile !== null ? (
+            <Friends
+              friend_name={friend_profile.username}
+              friend_profile={Friend1}
+              RightBar={true}
+            />
+          ) : null}
+          <p className="rightbar-heading"> FRIENDS</p>
+          <Friends
+            friend_name="Laura Fisher"
             friend_profile={Friend1}
             RightBar={true}
-          /> : null}
+          />
+          <Friends
+            friend_name="Sam Smith"
+            friend_profile={Friend2}
+            RightBar={true}
+          />
+          <Friends
+            friend_name="Monkey D Luffy"
+            friend_profile={Friend3}
+            RightBar={true}
+          />
         </div>
       </div>
     </>
